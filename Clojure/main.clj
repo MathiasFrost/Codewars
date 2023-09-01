ca76cf6f1eabc92e328c54bfb6eaeb4fe0921592
Mathias Løken <lakavi@mailfence.com>
2023-09-01 22:00:43 +0200

    Clojure

--- Clojure/main.clj
+++ Clojure/main.clj
@@ -1,13 +1,12 @@
-(require 'Histogram)
-(require 'BitCounter)
+(require 'histogram)
+(require 'bit-counter)
 
 (ns main)
 
 (defn -main
-([] (-main ""))
-([_]
+	  ([] (-main ""))
+	  ([_]
 
-    (println "Codewars Clojure")
-    (println (Histogram/hist "tpwaemuqxdmwqbqrjbeosjnejqorxdozsxnrgpgqkeihqwybzyymqeazfkyiucesxwutgszbenzvgxibxrlvmzihcb"))
-	   (println (format "Number of bits in 1234 is %d" (BitCounter/count-bits 1234)))
-))
+	   (println "Codewars Clojure")
+	   (println (histogram/hist "tpwaemuqxdmwqbqrjbeosjnejqorxdozsxnrgpgqkeihqwybzyymqeazfkyiucesxwutgszbenzvgxibxrlvmzihcb"))
+	   (println (format "Number of bits in 1234 is %d" (bit-counter/count-bits 1234)))))
---
+++ Clojure/src/bit_counter.clj
@@ -0,0 +1,12 @@
+(ns bit-counter)
+
+(defn count-bits [n]
+
+	  (def my-value (atom n))
+	  (def my-count (atom 0))
+	  (while (> @my-value 0)
+			 (do
+				 (reset! my-value (bit-and @my-value (- @my-value 1)))
+				 (swap! my-count inc)
+				 ))
+	  @my-count)
